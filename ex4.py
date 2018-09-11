94d7f794846bd8d2634c3a4b97ae7fe636dcaf98
omer <omer@localhost.localdomain>
2018-09-12 00:24:01 +0300

    homework for alex Basic Statement

--- ex4.py
+++ ex4.py
@@ -23,7 +23,7 @@
 #  
 ## Question No. 1
 y = 0
-arry = [17,1,12,54,23,9,21]
+arry = [17,1,12,54,23,9,21]     #establishing the array with the requested numbers..
 for x in arry:
 	if x >= 3 and x <= 20:
 		y +=x
@@ -38,17 +38,17 @@ sum1 = []
 while bool_var == True:
 	user_input = input("enter any number  you can think of ")
 	sum1.append(user_input)
-	if int(user_input) >= 10:
+	if int(user_input) >= 10:   #if the user input is bigger then 10
 		bool_var = False
 		b =0
-		for a in sum1:
+		for a in sum1:    #calculate the sum of the nums 
 			b +=int(a)
 		print(b)	
 	else:
 		c =0
-		for d in sum1:
+		for d in sum1:   #calculate the sim of the nums
 			c +=int(d)
-		if c >= 30:
+		if c >= 30:      #checking to see if the sum is not over 30
 			bool_var = False
 			print(c)	
 		else:
@@ -56,17 +56,17 @@ while bool_var == True:
 		
 
 ## Question No. 3
-arr_legenth = input("Enter your array length! ") 
+arr_legenth = input("Enter your array length! ") # user input array length 
 s1 = [] * int(arr_legenth)
-for arr in  range(int(arr_legenth)):
+for arr in  range(int(arr_legenth)):             #user input values into the array
     ArrVal = input("enter array value! ")
     s1.append(int(ArrVal)) 
-z = input("enter another random number! ")
-for L in range(3,int(arr_legenth)):
+z = input("enter another random number! ")       # user input the random num..
+for L in range(3,int(arr_legenth)):              # starting from the 4th item until the last one
 	if int(s1[L]) < 5:
-		s1[L]=  int(s1[L]) + int(z)
+		s1[L]=  int(s1[L]) + int(z)              #adding by random num
 	elif s1[L] >= 6 and s1[L] <= 10:
-		s1[L]=int(s1[L]) * int(z)
+		s1[L]=int(s1[L]) * int(z)                #multiplying by random num
 	elif s1[L] > 10:
-		s1[L]=int(s1[L]) ** int(z)
-print(s1)		 	 	 
+		s1[L]=int(s1[L]) ** int(z)               #powers by num random
+print(s1)		 	 	                         #print the array with the new values
